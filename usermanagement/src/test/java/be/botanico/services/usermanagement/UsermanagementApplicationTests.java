cf064a560813229f9e3a9c1c2b56f4169f151fd3
Bart Van Haute <bvanhaute@gmail.com>
2018-09-19 22:49:53 +0200

    added test with WebTestClient

--- usermanagement/src/test/java/be/botanico/services/usermanagement/UsermanagementApplicationTests.java
+++ usermanagement/src/test/java/be/botanico/services/usermanagement/UsermanagementApplicationTests.java
@@ -2,15 +2,29 @@ package be.botanico.services.usermanagement;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class UsermanagementApplicationTests {
+
+    private static final String DUMMY_USER_LIST = "[{\"lastName\":\"janssens\",\"firstNames\":[\"johan\",\"albert\"],\"id\":1,\"emails\":[\"johny@botanico.be\",\"johan.janssens@botanico.be\"]},{\"lastName\":\"waterslaeghers\",\"firstNames\":[\"zulma\",\"magda\"],\"id\":2,\"emails\":[\"zulma@botanico.be\"]},{\"lastName\":\"doe\",\"firstNames\":[\"john\"],\"id\":3,\"emails\":[\"john.doe@botanico.be\"]},{\"lastName\":\"smith\",\"firstNames\":[\"john\",\"zulma\"],\"id\":4,\"emails\":[\"john@botanico.be\",\"john.smith@botanico.be\"]}]";
+
+    @Autowired
+    private WebTestClient webTestClient;
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void testUsersList() {
+        this.webTestClient.get().uri("/users").exchange().expectStatus().isOk().expectBody(String.class).isEqualTo(DUMMY_USER_LIST);
     }
 
 }
